2c3613e0cfe2e5f442c7999120dd641c389e6d7c
escheive <scheiveerik@gmail.com>
2022-12-20 10:36:00 -0800

    added a Home Page and imported to app.js

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+
+// STYLES
+import './styles.css';
+
+export default function Home() {
+    return (
+        <main className="center-item">
+            
+            <h1>Welcome to Hiker's Hub!</h1>
+        </main>
+    )
+};
