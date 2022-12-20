f9c376ec3e7596c3a15bac39b61400196449b3f0
escheive <scheiveerik@gmail.com>
2022-12-20 10:17:49 -0800

    Created a basic nav with Links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 
+import Nav from './components/Nav';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
+      <Nav />
+
       <h1>Hikers Hub</h1>
+
     </div>
   );
 }
