d0e5b26af7a77e64e30376f1c7424f406497d979
escheive <scheiveerik@gmail.com>
2022-12-20 10:36:00 -0800

    added a Home Page and imported to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@
 // DEPENDENCIES
 import { Routes, Route } from 'react-router-dom';
 
+// PAGES
+import Home from './pages/Home';
 // COMPONENTS
 import Nav from './components/Nav';
 import Footer from './components/Footer';
@@ -19,11 +21,10 @@ function App() {
 
         <Route
           path='/'
-          element=
+          element={<Home />}
         />
 
       </Routes>
-      <h1>Hikers Hub</h1>
 
       <Footer />
     </div>
