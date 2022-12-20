d2b0514bb5eef78635e2cd69e532536cd7f01916
escheive <scheiveerik@gmail.com>
2022-12-20 10:28:59 -0800

    added a basic footer component and imported to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,31 @@
 
-import Nav from './components/Nav';
+// DEPENDENCIES
+import { Routes, Route } from 'react-router-dom';
 
+// COMPONENTS
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+// STYLES
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      
       <Nav />
+      
+      <Routes>
 
+        <Route
+          path='/'
+          element=
+        />
+
+      </Routes>
       <h1>Hikers Hub</h1>
 
+      <Footer />
     </div>
   );
 }
