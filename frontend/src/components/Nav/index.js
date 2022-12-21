f1872b49cc8a8e34c9a96da4925023fc14b1c888
escheive <scheiveerik@gmail.com>
2022-12-21 09:58:59 -0800

    installed bootstrap dependencies for react bootstrap

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -1,4 +1,10 @@
 
+// BOOTSTRAP
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+// DEPENDENCIES
 import { Link } from 'react-router-dom';
 import './style.css';
 
