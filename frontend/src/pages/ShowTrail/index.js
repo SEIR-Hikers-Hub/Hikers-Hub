e00c2ba69beb3ecae4c4d842c1211eae44cbdc7f
escheive <scheiveerik@gmail.com>
2022-12-30 17:11:46 -0800

    exported leave a review form to a component to help clean up code on showntrail page

--- frontend/src/pages/ShowTrail/index.js
+++ frontend/src/pages/ShowTrail/index.js
@@ -1,32 +1,17 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { deleteTrail, editTrail, createReview } from '../../utils/api';
+import { deleteTrail } from '../../utils/api';
+// COMPONENTS
+import CreateReviewForm from '../../components/CreateReviewForm';
 // STYLES
 import './styles.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 
 
 export default function ShowTrail({ getTrails, shownTrail, region, isLoggedIn }) {
 
     // STATE
-    const [formState, setFormState] = useState({})
-    const navigate = useNavigate()
     const initialState = []
     const [editDeleteOptions, setEditDeleteOptions] = useState(initialState)
-
-    const { id } = useParams()
-
-    function handleChange(event) {
-        setFormState({ ...formState, [event.target.id]: event.target.value })
-    }
-
-    const handleSubmit = (event) => {
-        // stops the page from reloading on submit
-        event.preventDefault();
-        createReview(id, formState);
-        navigate('/');
-    };
 
     // use effect to conditionally render edit and delete functionality if signed in
     useEffect(() => {
@@ -81,63 +66,7 @@ export default function ShowTrail({ getTrails, shownTrail, region, isLoggedIn })
             <h2>Leave a Review!</h2>
             <br></br>
 
-                <Form onSubmit={handleSubmit} className="mx-auto">
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Title:</Form.Label>
-                        <Form.Control
-                            type="text"
-                            id="title"
-                            title="title"
-                            onChange={handleChange}
-                            placeholder="Title for review goes here"
-                            required />
-                    </Form.Group>
-
-
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Rating:</Form.Label>
-                        <Form.Select
-                            id="rating"
-                            onChange={handleChange}
-                            required>
-                            <option>Rate this hike</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                        </Form.Select>
-                    </Form.Group>
-
-
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Content:</Form.Label>
-                        <Form.Control
-                            type="text"
-                            id="content"
-                            name="content"
-                            onChange={handleChange}
-                            placeholder="Leave your review here" />
-                    </Form.Group>
-
-
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Reviewer:</Form.Label>
-                        <Form.Control
-                            type="text"
-                            id="reviewer"
-                            name="reviewer"
-                            onChange={handleChange}
-                            placeholder="Your name" />
-                    </Form.Group>
-
-                    <br></br>
-
-                    <Button variant="primary" type="submit">Post Review</Button>
-
-                </Form>
+                <CreateReviewForm />
             
             <br></br><br></br><br></br>
 
